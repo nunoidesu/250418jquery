338ddb8e20d2b97edef1d09adf4d3a528eb810d9
nunoidesu <167516695+nunoidesu@users.noreply.github.com>
2025-04-18 18:24:41 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.getElementById("myButton").addEventListener("click",function(){
+//     alert("ボタンを押したよ！");
+// });
+
+// jQuery
+$(function(){
+    $("#myButton").click(function(){
+        alert("ボタンを押したぞ！");
+    });
+});
+
